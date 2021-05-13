7982dde495e7944bae3fe5fbfe9f05270021de2e
Ananya Mehra <67919970+ANANYA162020@users.noreply.github.com>
2021-05-13 11:59:43 -0700

    Add files via upload

---
+++ test16.py
@@ -0,0 +1,15 @@
+import requests
+import json
+city="Indianapolis"
+country="US"
+api_key = "###"
+
+weather_url = requests.get(f'http://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city},{country}&units=imperial')
+
+weather_data = weather_url.json()
+
+
+temp = round(weather_data['main']['temp'])
+humidity = weather_data['main']['humidity']
+wind_speed = weather_data['winf']['speed']
+
